01fa960c8ecc1857bb98d59c7e5161a77435c547
1m1 <imi@1m1.io>
2026-04-23 20:50:39 -0600

    .

---
+++ src/LoopOSMainAgent.jl
@@ -0,0 +1,31 @@
+module LoopOSMainAgent
+
+const T = Float64
+
+using TheoryOfGod: 𝕋, t, ○
+const Ω = 𝕋()
+
+using StaticArrays
+using TheoryOfGodgod: god
+const GOD = god(;
+    d = SA[zero(T)],
+    t = t(),
+    ẑeroμ = SA[○, ○, ○],
+    ôneμ = SA[○, ○, ○],
+    ρ = (T(0.01),T(0.01),T(0.01)),
+    ♯ = (10^3, 10^3),
+)
+
+using LoopOSMainAgentTextCommunication
+const ROUTERTEXTLOCATION = "ipc://./routertext" # change to tcp if on separate machines
+const PUBTEXTLOCATION = "ipc://./pubtext" # change to tcp if on separate machines
+const TEXTCOMMUNICATIONTASK = LoopOSMainAgentTextCommunication.init(ROUTERTEXTLOCATION, PUBTEXTLOCATION)
+
+using LoopOSMainAgentgodCommunication
+const CREATETOGLOCATION = "ipc://./createtog" # change to tcp if on separate machines
+const OBSERVETOGLOCATION = "ipc://./observetog" # change to tcp if on separate machines
+const CREATETOGTASK, OBSERVETOGTASK = LoopOSMainAgentgodCommunication.init(CREATETOGLOCATION, OBSERVETOGLOCATION, Ω)
+
+using LoopOSAgentManagement
+
+end
